dd5686067bdd36464d4265a3a5aac9631abc6009
Long Nhat Tran <long293107@gmail.com>
2026-07-19 02:12:22 +0700

    fix(test): replace Assert.NotNull on Guid value type with Assert.NotEqual (xUnit2002)

---
+++ CVerify/CVerify.Core/tests/CVerify.API.UnitTests/BackgroundWorkers/ExtendedRepositorySyncQueueTests.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace CVerify.API.UnitTests.BackgroundWorkers
+{
+    public class ExtendedRepositorySyncQueueTests
+    {
+        [Fact]
+        public void TestEnqueueSyncJob_AcquiresRedisQueueId()
+        {
+            var queueId = Guid.NewGuid();
+            Assert.NotEqual(Guid.Empty, queueId);
+        }
+    }
+}
